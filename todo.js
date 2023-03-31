14f78be1e089e7c78976ce9710a077805690a5ce
Keezer83 <126595440+Keezer83@users.noreply.github.com>
2023-03-31 10:29:00 -0300

    class changes in the DOM from checkbok listener

--- todo.js
+++ todo.js
@@ -1,15 +1,36 @@
 document.getElementById("addItem").addEventListener("click", addToDo);
+const toDoList = document.querySelector(".listToComplete");
+let toDoOption = document.querySelector(".toDoInput");
 
 function addToDo() {
-  const toDoList = document.getElementById("listToComplete");
-  let toDoOption = document.getElementById("toDoInput");
-  let newToDo = document.createElement("li");
-  newToDo.setAttribute("id", toDoOption.value);
-  newToDo.appendChild(document.createTextNode(toDoOption.value));
-  toDoList.appendChild(newToDo);
+  const listItem = document.createElement("li");
+  listItem.textContent = toDoOption.value;
+  const listCheckbox = document.createElement("input");
+  listCheckbox.setAttribute("type", "checkbox");
+  listCheckbox.addEventListener("change", function () {
+    if (listCheckbox.checked === true) {
+      listItem.setAttribute("class", "itemCompleted");
+    } else {
+      listItem.setAttribute("class", "");
+    }
+    console.log(listCheckbox.checked);
+  });
+  toDoOption.value = "";
+  toDoList.appendChild(listItem);
+  listItem.appendChild(listCheckbox);
+
+  // toDoList.innerHTML += `<li id="${toDoOption.value}"><input type="checkbox" class="pending" id="${toDoOption.value}"/>${toDoOption.value}</li>`;
+  // toDoOption.value = "";
+
+  //   Array.from(
+  //     document
+  //       .querySelectorAll(".pending")
+  //       .addEventListener("change", completeToDo)
+  //   );
+  //   function completeToDo() {
+  //     const completedItem = document.querySelectorAll("li");
+  //     const chkCompletedItem = Array.from(
+  //       document.getElementsByClassName("input")
+  //     );
+  //   }
 }
-const listItems = document.querySelectorAll(".listToComplete li");
-listItems.forEach(function (addToDo) {
-  let toDoCheckbox = document.createElement("input:checkbox");
-  toDoCheckbox.appendChild(toDoCheckbox);
-});
